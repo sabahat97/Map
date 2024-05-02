e58642216a77d8e8a86b7ba188279fa9392369ac
syeda <sbtshakeel61@gmail.com>
2024-05-02 19:25:36 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,12 @@
+//type number
+let array =[1, 2, 3, 4, 5]
+console.log (array.map((number)=>{
+    return number*2;
+}));
+
+//type string
+let array1 = [""]
+console.log(array1.map((string)=>{
+    return "daniya is my friend"
+}))
+
---
+++ main.ts
@@ -0,0 +1,6 @@
+let namee = ["saabahaat"]  //mistake of aa
+let a = namee.map((string)=>
+{  return "sabahat" //corrected the misatke of aa
+
+})
+console.log(a)
